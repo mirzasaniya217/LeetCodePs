23547548a209f768eff9bd7972e1601135e1ea72
MIRZA SANIYA <mirzasaniya1354@gmail.com>
2026-07-16 19:49:38 +0530

    Time: 1 ms (99.33%), Space: 47.1 MB (8.37%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int zeros = 0;
+        for (int num : arr) {
+            if (num == 0)
+               zeros++;
+        }
+        int i = arr.length - 1;
+        int j = arr.length + zeros - 1;
+        while(i >= 0) {
+            if (j < arr.length)
+                arr[j] = arr[i];
+            if (arr[i] == 0){
+                j--;
+                if (j < arr.length)
+                   arr[j] = 0;
+            }
+            i--;
+            j--;
+        }
+        
+    }
+}
